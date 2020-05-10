24c8f95e2625feed4f48733016b1aa3fcb329da2
szoonaj <szonaj98@gmail.com>
2020-05-10 22:36:20 +0200

    first commit

---
+++ Chuckle_Tanks_Tangle/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float m_DampTime = 0.2f;                 // Approximate time for the camera to refocus.
+    public float m_ScreenEdgeBuffer = 4f;           // Space between the top/bottom most target and the screen edge.
+    public float m_MinSize = 6.5f;                  // The smallest orthographic size the camera can be.
+    [HideInInspector] public Transform[] m_Targets;    // All the targets the camera needs to encompass.
+
+
+    private Camera m_Camera;                        
+    private float m_ZoomSpeed;                      
+    private Vector3 m_MoveVelocity;                 
+    private Vector3 m_DesiredPosition;              // The position the camera is moving towards.
+
+
+    private void Awake()
+    {
+        m_Camera = GetComponentInChildren<Camera>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Move the camera towards a desired position.
+        Move();
+        // Change the size of the camera based.
+        Zoom();
+    }
+
+
+    private void Move()
+    {
+        FindAveragePosition();
+
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
+        
+        // Go through all the targets and add their positions together.
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            // If the target isn't active, go on to the next one.
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            averagePos += m_Targets[i].position;
+            numTargets++;
+        }
+
+        if (numTargets > 0)
+            averagePos /= numTargets;
+
+        averagePos.y = transform.position.y;
+
+        m_DesiredPosition = averagePos;
+    }
+
+
+    private void Zoom()
+    {
+        // Find the required size based on the desired position and smoothly transition to that size.
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+    }
+
+
+    private float FindRequiredSize()
+    {
+        // Find the position the camera rig is moving towards in its local space.
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        float size = 0f;
+
+        // Go through all the targets...
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
+
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
+        }
+        
+        size += m_ScreenEdgeBuffer;
+
+        // Make sure the camera's size isn't below the minimum.
+        size = Mathf.Max(size, m_MinSize);
+
+        return size;
+    }
+
+
+    public void SetStartPositionAndSize()
+    {
+        FindAveragePosition();
+
+        transform.position = m_DesiredPosition;
+
+        m_Camera.orthographicSize = FindRequiredSize();
+    }
+}
---
+++ Chuckle_Tanks_Tangle/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,217 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public int m_NumRoundsToWin = 5;        
+    public float m_StartDelay = 3f;         // The delay between the start of RoundStarting and RoundPlaying phases.
+    public float m_EndDelay = 3f;           // The delay between the end of RoundPlaying and RoundEnding phases.
+    public CameraControl m_CameraControl;   
+    public Text m_MessageText;              
+    public GameObject m_TankPrefab;         
+    public TankManager[] m_Tanks;           // A collection of managers for enabling and disabling different aspects of the tanks.
+
+
+    private int m_RoundNumber;              
+    private WaitForSeconds m_StartWait;     
+    private WaitForSeconds m_EndWait;       
+    private TankManager m_RoundWinner;
+    private TankManager m_GameWinner;       
+
+
+    private void Start()
+    {
+        m_StartWait = new WaitForSeconds(m_StartDelay);
+        m_EndWait = new WaitForSeconds(m_EndDelay);
+
+        SpawnAllTanks();
+        SetCameraTargets();
+
+        StartCoroutine(GameLoop());
+    }
+
+
+    private void SpawnAllTanks()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].m_Instance =
+                Instantiate(m_TankPrefab, m_Tanks[i].m_SpawnPoint.position, m_Tanks[i].m_SpawnPoint.rotation) as GameObject;
+            m_Tanks[i].m_PlayerNumber = i + 1;
+            m_Tanks[i].Setup();
+        }
+    }
+
+
+    private void SetCameraTargets()
+    {
+        Transform[] targets = new Transform[m_Tanks.Length];
+
+        for (int i = 0; i < targets.Length; i++)
+        {
+            targets[i] = m_Tanks[i].m_Instance.transform;
+        }
+
+        m_CameraControl.m_Targets = targets;
+    }
+
+
+    private IEnumerator GameLoop()
+    {
+        yield return StartCoroutine(RoundStarting());
+        yield return StartCoroutine(RoundPlaying());
+        yield return StartCoroutine(RoundEnding());
+
+        if (m_GameWinner != null)
+        {
+            // If there is a game winner, restart the level.
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            StartCoroutine(GameLoop());
+        }
+    }
+
+
+    private IEnumerator RoundStarting()
+    {
+        ResetAllTanks();
+        DisableTankControl();
+
+        m_CameraControl.SetStartPositionAndSize();
+
+        m_RoundNumber++;
+        m_MessageText.text = "ROUND " + m_RoundNumber;
+
+        yield return m_StartWait;
+    }
+
+
+    private IEnumerator RoundPlaying()
+    {
+        EnableTankControl();
+
+        m_MessageText.text = string.Empty;
+
+        while (!OneTankLeft())
+        {
+            yield return null;
+        }
+    }
+
+
+    private IEnumerator RoundEnding()
+    {
+        DisableTankControl();
+
+        m_RoundWinner = null;
+        m_RoundWinner = GetRoundWinner();
+
+        if (m_RoundWinner != null)
+        {
+            m_RoundWinner.m_Wins++;
+        }
+
+        m_GameWinner = GetGameWinner();
+
+        string message = EndMessage();
+        m_MessageText.text = message;
+
+        yield return m_EndWait;
+    }
+
+
+    private bool OneTankLeft()
+    {
+        int numTanksLeft = 0;
+
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Instance.activeSelf)
+                numTanksLeft++;
+        }
+
+        return numTanksLeft <= 1;
+    }
+
+    // This function is called with the assumption that 1 or fewer tanks are currently active.
+    private TankManager GetRoundWinner()
+    {
+        // go through all the tanks...
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            if (m_Tanks[i].m_Instance.activeSelf)
+                return m_Tanks[i];
+        }
+        
+        // If none of the tanks are active it is a draw so return null.
+        return null;
+    }
+
+
+    private TankManager GetGameWinner()
+    {
+        // go through all the tanks...
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            // ... and if one of them has enough rounds to win the game, return it.
+            if (m_Tanks[i].m_Wins == m_NumRoundsToWin)
+                return m_Tanks[i];
+        }
+
+        // If no tanks have enough rounds to win, return null.
+        return null;
+    }
+
+
+    private string EndMessage()
+    {
+        // default option
+        string message = "DRAW!";
+
+        if (m_RoundWinner != null)
+            message = m_RoundWinner.m_ColoredPlayerText + " WINS THE ROUND!";
+
+        message += "\n\n\n\n";
+
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            message += m_Tanks[i].m_ColoredPlayerText + ": " + m_Tanks[i].m_Wins + " WINS\n";
+        }
+
+        if (m_GameWinner != null)
+            message = m_GameWinner.m_ColoredPlayerText + " WINS THE GAME!";
+
+        return message;
+    }
+
+
+    private void ResetAllTanks()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].Reset();
+        }
+    }
+
+
+    private void EnableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].EnableControl();
+        }
+    }
+
+
+    private void DisableTankControl()
+    {
+        for (int i = 0; i < m_Tanks.Length; i++)
+        {
+            m_Tanks[i].DisableControl();
+        }
+    }
+}
